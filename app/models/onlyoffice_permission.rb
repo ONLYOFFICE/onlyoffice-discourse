395d16ba27f249b4a93ace0b7febf7ea8c11ea0f
Alexander Salyakhov <alexander.salyakhov@onlyoffice.com>
2025-12-02 17:40:30 +0300

    feat: add permissions schema

---
+++ app/models/onlyoffice_permission.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+module Onlyoffice
+  class Permission < ActiveRecord::Base
+    self.table_name = 'onlyoffice_permissions'
+
+    belongs_to :upload
+    belongs_to :user
+
+    validates :upload_id, presence: true
+    validates :user_id, presence: true
+    validates :permission_type, inclusion: { in: %w[viewer editor] }
+    validates :user_id, uniqueness: { scope: :upload_id }
+
+    VIEWER = 'viewer'
+    EDITOR = 'editor'
+  end
+end
---
+++ db/migrate/20250119160800_create_onlyoffice_permissions.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+class CreateOnlyofficePermissions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :onlyoffice_permissions do |t|
+      t.integer :upload_id, null: false
+      t.integer :user_id, null: false
+      t.string :permission_type, null: false, default: 'viewer'
+      t.timestamps
+    end
+
+    add_index :onlyoffice_permissions, [:upload_id, :user_id], unique: true
+    add_index :onlyoffice_permissions, :upload_id
+    add_index :onlyoffice_permissions, :user_id
+  end
+end
